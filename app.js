e5490fcbbceab0ea9348d4f1841f760424d110fa
Anaflavia <anaflaviadiazmartel5a@gmail.com>
2018-08-02 17:15:50 -0500

    search ready

--- app.js
+++ app.js
@@ -16,8 +16,6 @@ const filterBar = document.getElementById("filter-bar");
 const listRestaurants = document.getElementById("list-restaurants");
 let allRestaurants;
 const modalRestaurant = document.getElementById("modal-restaurant-description");
-const btnModal = document.getElementById("btn-modal");
-const btnCloseModal = document.getElementById("close-modal");
 
 window.onload = () => {
 	setTimeout(() => {
@@ -67,7 +65,7 @@ const showRestaurants = (restaurants) => {
 	restaurants.map(restaurant => {
 		listRestaurants.innerHTML +=
 	`
-		<nav class="level is-mobile">
+		<nav class="level is-mobile" id="${restaurant.id}" onclick="createModal('${restaurant.id}')">
 			<div class="level-left">
 				<div class="level-item">
 					<figure class="image is-48x48">
@@ -88,53 +86,78 @@ const showRestaurants = (restaurants) => {
 	});
 }
 
+const closeModal = () => {
+	modalRestaurant.style.display = "none";
+}
+
+const filterInModal = (idRestaurant) => {
+	const filterRestaurants = allRestaurants.filter(restaurant => {
+		return restaurant.id === idRestaurant;
+	});
+	return filterRestaurants;
+}
+
+const createModal = (idRestaurant) => {
+	modalRestaurant.innerHTML = "";
+	const onlyRestaurant = filterInModal(idRestaurant);
+	onlyRestaurant.map(restaurant => {
+		modalRestaurant.innerHTML =
+		`
+		<div class="modal is-flex-mobile">
+			<div class="modal-background"></div>
+			<div class="modal-card">
+				<header class="modal-card-head">
+					<p class="modal-card-title">${restaurant.name}</p>
+					<button class="delete" id="close-modal" aria-label="close" onclick="closeModal()"></button>
+				</header>
+				<section class="modal-card-body">
+					<p><strong>Tipo: </strong>${restaurant.type}</p>
+					<p><strong>Ubicación: </strong>${restaurant.address}</p>
+				</section>
+				<footer class="modal-card-foot">
+					<a class="button is-warning subtitle is-5" href="${restaurant.link}" target="_blank">Visitar</a>
+				</footer>
+			</div>
+		</div>
+		`
+	});
+	modalRestaurant.style.display = "block";
+}
+
 inputSearch.addEventListener("input", (e) => {
 	const search = searchByFilter(e.target.value);
 	showRestaurants(search);
 });
 filterParrilla.addEventListener("click", () => {
-	getRestaurantByFilter("Carnes y Parrillas")
+	getRestaurantByFilter("Carnes y Parrillas");
 });
 filterChifa.addEventListener("click", () => {
-	getRestaurantByFilter("Chifa")
+	getRestaurantByFilter("Chifa");
 });
 filterCriolla.addEventListener("click", () => {
-	getRestaurantByFilter("Comida Criolla")
+	getRestaurantByFilter("Comida Criolla");
 });
 filterSelva.addEventListener("click", () => {
-	getRestaurantByFilter("Comida Selvática")
+	getRestaurantByFilter("Comida Selvática");
 });
 filterSushi.addEventListener("click", () => {
-	getRestaurantByFilter("Comida Japonesa")
+	getRestaurantByFilter("Comida Japonesa");
 });
 filterInternacional.addEventListener("click", () => {
-	getRestaurantByFilter("Comida Peruana Internacional")
+	getRestaurantByFilter("Comida Peruana Internacional");
 });
 filterFusion.addEventListener("click", () => {
-	getRestaurantByFilter("Fusión")
+	getRestaurantByFilter("Fusión");
 });
 filterPostre.addEventListener("click", () => {
-	getRestaurantByFilter("Pastelería")
+	getRestaurantByFilter("Pastelería");
 });
 filterPescado.addEventListener("click", () => {
-	getRestaurantByFilter("Pescados y Mariscos")
+	getRestaurantByFilter("Pescados y Mariscos");
 });
 filterBar.addEventListener("click", () => {
-	getRestaurantByFilter("Restaurante Bar")
+	getRestaurantByFilter("Restaurante Bar");
 });
 filterSandwich.addEventListener("click", () => {
-	getRestaurantByFilter("Sandwicherias y Cafés")
+	getRestaurantByFilter("Sandwicherias y Cafés");
 });
-btnModal.addEventListener("click", () => {
-	modalRestaurant.style.display = "block";
-});
-btnCloseModal.addEventListener("click", () => {
-	modalRestaurant.style.display = "none";
-});
-
-// filter.addEventListener("click", () => console.log(""));
-// filter.addEventListener("click", () => console.log(""));
-// filter.addEventListener("click", () => console.log(""));
-// filter.addEventListener("click", () => console.log(""));
-// filter.addEventListener("click", () => console.log(""));
-// filter.addEventListener("click", () => console.log(""));
